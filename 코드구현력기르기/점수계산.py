94f1ecb9a2b2a295d81ec4cb9c46a4481fc24dca
solnip <minji5276@gmail.com>
2022-02-23 21:36:58 +0900

    Add Python File [점수계산.py]

---
+++ 코드구현력기르기/점수계산.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+N=int(input())
+a=list(map(int, input().split()))
+sum=0
+cnt=0
+for x in a:
+    if x==1:
+        cnt+=1
+        sum+=cnt
+    else:
+        cnt=0
+print(sum)

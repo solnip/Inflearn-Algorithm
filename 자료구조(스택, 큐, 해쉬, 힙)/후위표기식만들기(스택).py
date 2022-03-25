299f0b3f61c83fc775ff6c376c41f3bae19342ec
solnip <minji5276@gmail.com>
2022-03-25 20:54:25 +0900

    스택, postfix이해, while문

---
+++ 자료구조(스택, 큐, 해쉬, 힙)/후위표기식만들기(스택).py
@@ -0,0 +1,37 @@
+'''
+중위표기식이 입력되면 후위표기식으로 변환하는 프로그램을 작성하세요.
+중위표기식은 우리가 흔히 쓰은 표현식입니다. 즉 3+5 와 같이 연산자가 피연산자 사이에 있
+으면 중위표기식입니다.
+후위표기식은 35+ 와 같이 연산자가 피연산자 뒤에 있는 표기식입니다.
+예를 들어 중위표기식이 3+5*2 를 후위표기식으로 표현하면 352*+ 로 표현됩니다.
+만약 다음과 같이 연산 최우선인 괄호가 표현된 식이라면
+(3+5)*2 이면 35+2* 로 바꾸어야 합니다.
+'''
+import sys
+sys.stdin=open("input.txt", "rt")
+a=input()
+stack=[]
+res=''
+# x가 중위식 탐색
+for x in a:
+    if x.isdecimal():
+        res+=x
+    else:
+        if x=='(':
+            stack.append(x)
+        elif x=='*' or x=='/':
+            while stack and (stack[-1]=='*' or stack[-1]=='/'):
+                res+=stack.pop()
+            stack.append(x)
+        # 괄호 안의 연산자
+        elif x=='+' or x=='-':
+            while stack and stack[-1]!='(':
+                res+=stack.pop()
+            stack.append(x)
+        elif x==')':
+            while stack and stack[-1]!='(':
+                res += stack.pop()
+            stack.pop()
+while stack:
+    res+=stack.pop()
+print(res)

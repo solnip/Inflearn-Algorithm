b82b36a9cf5ae0663a0e83b6511efb39088c313d
solnip <minji5276@gmail.com>
2022-03-26 10:45:02 +0900

    스택, postfix

---
+++ 자료구조(스택, 큐, 해쉬, 힙)/후위연산하기(스택).py
@@ -0,0 +1,31 @@
+'''
+후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
+만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 21입니다.
+'''
+import sys
+sys.stdin=open("input.txt", "rt")
+a=input()
+stack=[]
+# x가 후위식 탐색
+for x in a:
+    if x.isdecimal():
+        # x는 문자이므로 int화 시켜서 넣어줘야함
+        stack.append(int(x))
+    else:
+        if x=='+':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2+n1)
+        elif x=='-':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2-n1)
+        elif x=='*':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2*n1)
+        elif x=='/':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2/n1)
+print(stack[0])
